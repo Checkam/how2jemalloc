73a538ef6cd358ff10b013242fac9b51eb1462b5
checkam <maxencedespres@gmail.com>
2022-03-02 12:30:27 +0100

    Update use-after-free

--- jemalloc_5.2.1/use-after-free.c
+++ jemalloc_5.2.1/use-after-free.c
@@ -5,15 +5,25 @@
 
 int main(int agrc, char **argv)
 {
+   // Alloc for fun	
+   char *first = malloc(sizeof(char) * 8);
+   char *second = malloc(sizeof(char) * 8);
+   char *third = malloc(sizeof(char) * 8);
+   char *fourth = malloc(sizeof(char) * 8);
+   printf("first %p : second : %p, third : %p, fourth : %p\n", first, second, third, fourth);
+   // LIFO, STACK
+   
+  free(second);
+  free(fourth);
+  
   printf("[+] Alloc buffer1 size of 8\n");
   char *buffer1 = malloc(sizeof(char) * 8);
   memset(buffer1, 'A', 7);
   printf("[%p] buffer1 : %s\n", buffer1, buffer1);
 
   printf("\n[+] Free buffer1\n");
-  free(buffer1);
 
-  // Add gabage malloc
+  free(buffer1);
 
   printf("\n[+] Alloc buffer2 size of 8\n");
   char *buffer2 = malloc(sizeof(char) * 8);
